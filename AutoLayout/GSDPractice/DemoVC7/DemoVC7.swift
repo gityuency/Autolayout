9c5c0f9ace4b3180d87b0624b1091db7b453fdc6
yuency yang <ext.yuency.yang@ef.com>
2017-09-22 10:13:28 +0800

    _ 添加了DemoVC7

---
+++ AutoLayout/GSDPractice/DemoVC7/DemoVC7.swift
@@ -0,0 +1,17 @@
+//
+//  DemoVC7.swift
+//  AutoLayout
+//
+//  Created by yuency on 22/09/2017.
+//  Copyright © 2017 yuency. All rights reserved.
+//
+
+import UIKit
+
+class DemoVC7: ViewsViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ AutoLayout/GSDPractice/DemoVC7/DemoVC7Cell2.swift
@@ -0,0 +1,38 @@
+//
+//  DemoVC7Cell2.swift
+//  AutoLayout
+//
+//  Created by yuency on 22/09/2017.
+//  Copyright © 2017 yuency. All rights reserved.
+//
+
+import UIKit
+
+class DemoVC7Cell2: UITableViewCell {
+
+    
+    var model: DemoVC7Model?
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier
+        )
+    }
+    
+
+}
